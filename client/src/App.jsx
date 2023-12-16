98c47335abf2f1e225aa520d8fea6d5d2be85680
Julian Houle-T <jhouletymeczko@gmail.com>
2023-12-16 06:06:32 -0500

    restarting to make a different project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,8 @@
 import "./App.css";
-import Question from "./components/Question";
+
 import "./reset.css";
 function App() {
-  return (
-    <div className="main">
-      <h1>Questions</h1>
-      <div className="questions">
-        <Question
-          title="awdwadwadwadwad"
-          answer="lorem*dwadddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa dadsda ad asd as das sd as da ssd "
-          plus="1"
-          minus="2"
-        ></Question>
-        <Question
-          title="awdwadwadwadwad"
-          answer="lorem*dwadddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa dadsda ad asd as das sd as da ssd "
-          plus="3"
-          minus="4"
-        ></Question>
-        <Question
-          title="awdwadwadwadwad"
-          answer="lorem*dwadddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa dadsda ad asd as das sd as da ssd "
-          plus="5"
-          minus="6"
-        ></Question>
-        <Question
-          title="Stan"
-          answer="lorem*dwadddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa dadsda ad asd as das sd as da ssd "
-          plus="7"
-          minus="8"
-        ></Question>
-      </div>
-    </div>
-  );
+  return <h3>daw</h3>;
 }
 
 export default App;
