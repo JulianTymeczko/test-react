39bab4e8759dfa1c893822ac9f16020e4fba3347
Julian Houle-T <jhouletymeczko@gmail.com>
2023-12-16 06:05:27 -0500

    going to different practice project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,35 @@ import Question from "./components/Question";
 import "./reset.css";
 function App() {
   return (
-    <Question
-      title="awdwadwadwadwad"
-      answer="lorem*dwadddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa dadsda ad asd as das sd as da ssd "
-    ></Question>
+    <div className="main">
+      <h1>Questions</h1>
+      <div className="questions">
+        <Question
+          title="awdwadwadwadwad"
+          answer="lorem*dwadddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa dadsda ad asd as das sd as da ssd "
+          plus="1"
+          minus="2"
+        ></Question>
+        <Question
+          title="awdwadwadwadwad"
+          answer="lorem*dwadddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa dadsda ad asd as das sd as da ssd "
+          plus="3"
+          minus="4"
+        ></Question>
+        <Question
+          title="awdwadwadwadwad"
+          answer="lorem*dwadddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa dadsda ad asd as das sd as da ssd "
+          plus="5"
+          minus="6"
+        ></Question>
+        <Question
+          title="Stan"
+          answer="lorem*dwadddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa dadsda ad asd as das sd as da ssd "
+          plus="7"
+          minus="8"
+        ></Question>
+      </div>
+    </div>
   );
 }
 
--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
 import "./question.css";
-export default function Question({ title, answer }) {
+export default function Question({ title, answer, plus, minus }) {
   const [showAnswer, setShowAnswer] = useState("");
   useEffect(() => {
     if (showAnswer == "") {
-      document.getElementById("plus").setAttribute("style", "display: intial");
-      document.getElementById("minus").setAttribute("style", "display: none");
+      document
+        .getElementById(`${plus}`)
+        .setAttribute("style", "display: intial");
+      document
+        .getElementById(`${minus}`)
+        .setAttribute("style", "display: none");
     } else if (showAnswer == "shown") {
       document
-        .getElementById("minus")
+        .getElementById(`${minus}`)
         .setAttribute("style", "display: initial");
-      document.getElementById("plus").setAttribute("style", "display: none");
+      document.getElementById(`${plus}`).setAttribute("style", "display: none");
     }
   }, [showAnswer]);
   return (
@@ -33,7 +37,7 @@ export default function Question({ title, answer }) {
             height="16"
             width="14"
             viewBox="0 0 448 512"
-            id="plus"
+            id={`${plus}`}
           >
             <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
           </svg>
@@ -42,7 +46,7 @@ export default function Question({ title, answer }) {
             height="16"
             width="14"
             viewBox="0 0 448 512"
-            id="minus"
+            id={`${minus}`}
           >
             <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z" />
           </svg>
