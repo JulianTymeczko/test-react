d2afacc53d97451b12588dbc4834e6b4fe7c85a2
Julian Houle-T <jhouletymeczko@gmail.com>
2023-12-17 07:02:58 -0500

    trying material UI some more

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
-
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./reset.css";
 function App() {
-  return <h3>daw</h3>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
